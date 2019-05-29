999c08dc4fc459035432fe371ea468ba6004e66d
MrT3313 <rturgeon13@gmail.com>
2019-05-29 15:24:07 -0500

    figing routing issues

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,5 @@ const server = require ('./api/server.js')
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-    console.log(`\n=== HOMEBASE API Listening on http://localhost:${PORT} ===\n`);
+    console.log(`\n=== WEB DB 3 -- API Listening on http://localhost:${PORT} ===\n`);
 })
